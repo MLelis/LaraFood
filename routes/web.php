daf3e4383d214d2d7657c68490d4492bed8b013b
Marcus Lelis <marcuslelis95@gmail.com>
2021-07-09 10:55:50 -0300

    Módulo 04 Permissões

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::prefix('admin')
             ->namespace('Admin')
             ->group(function () {
 
+
+    /**
+     * Routes Profiles
+     */
+    Route::any('permissions/search', 'ACL\PermissionController@search')->name('permissions.search');
+    Route::resource('permissions', 'ACL\PermissionController');
+
+
     /**
      * Routes Profiles
      */
