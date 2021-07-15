c40f7da35a0ca9a59db93358f832e8045fd953c5
Marcus Lelis <marcuslelis95@gmail.com>
2021-07-15 20:47:10 -0300

    Módulo 13 - Produtos x Categorias

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,7 +13,7 @@ class Product extends Model
 
     public function categories()
     {
-        $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class);
     }
 
     public function search($filter = null)
@@ -23,5 +23,26 @@ class Product extends Model
                         ->paginate();
 
         return $results;
+    }
+
+    /**
+     * Recuperar categorias disponiveis
+     */
+    public function categoriesAvailable($filter = null)
+    {
+        $categories = Category::whereNotIn('categories.id', function($query) {
+            $query->select('category_product.category_id');
+            $query->from('category_product');
+            $query->whereRaw("category_product.product_id={$this->id}");
+        })
+        ->where(function ($queryFilter) use ($filter) {
+            if($filter){
+                $queryFilter->where('categories.name', 'LIKE', "%{$filter}%");
+            }
+        })
+        ->paginate();
+
+
+        return $categories;
     }
 }
