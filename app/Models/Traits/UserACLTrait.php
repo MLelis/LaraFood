f997749db2743c828457bfec81075fb4b8e17d16
Marcus Lelis <marcuslelis95@gmail.com>
2021-08-12 17:42:26 -0300

    Módulo 18 - ACL Authorization (Roles X Permissions

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -3,17 +3,49 @@
 namespace App\Models\Traits;
 
 use App\Models\Permission;
+use App\Models\Tenant;
 
 trait UserACLTrait
 {
-    public function permissions()
+    public function permissions(): array
     {
-        $tenant = $this->tenant;
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        $permissions = [];
+        foreach($permissionsRole as $permissionRole){
+            if(in_array($permissionRole, $permissionsPlan)){
+                array_push($permissions, $permissionRole);
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan(): array
+    {
+        // $tenant = $this->tenant;
+        // $plan = $tenant->plan;
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
         $plan = $tenant->plan;
 
         $permissions = [];
         foreach($plan->profiles as $profile){
             foreach($profile->permissions as $permission){
+                array_push($permissions, $permission->name);
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsRole(): array
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        $permissions = [];
+        foreach($roles as $role){
+            foreach($role->permissions as $permission){
                 array_push($permissions, $permission->name);
             }
         }
